d6a2d6e17bb87d736e3da738148ee0f09f277dc1
Nora Granahan <namgranahan@gmail.com>
2018-05-14 19:59:11 -0400

    set up MySQL database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Slider');
