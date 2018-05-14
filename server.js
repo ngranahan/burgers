2a0575bace19039ac4ccf1aacb0492a69c1e3538
Nora Granahan <namgranahan@gmail.com>
2018-05-14 19:35:30 -0400

    Created directory and server

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3001
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: https://localhost:" + PORT); 
+});
