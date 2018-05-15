f234fd0ca704fd23d2a816148b98f4edec2ccff0
Nora Granahan <namgranahan@gmail.com>
2018-05-14 21:15:58 -0400

    Added models and views

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    addOne: function (cols, vals, cb) {
+        orm.addOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
